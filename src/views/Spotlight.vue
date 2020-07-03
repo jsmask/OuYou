bb9d76d6c3f85bc9975551bdef7c5002d2704848
jsmask <2674800533@qq.com>
2020-07-03 17:37:03 +0800

    update

--- src/views/Spotlight.vue
+++ src/views/Spotlight.vue
@@ -8,7 +8,7 @@
 export default {
   data() {
     return {
-       txt:"SpotLight"
+      txt: "SpotLight"
     };
   }
 };
@@ -20,11 +20,21 @@ export default {
   min-height: 100%;
   padding: 120px 0;
   text-align: center;
-  background: #333;
   overflow: hidden;
+  background-color: #333;
+  background-image: linear-gradient(
+      rgba(255, 255, 255, 0.1) 1px,
+      transparent 1px
+    ),
+    linear-gradient(90deg, rgba(255, 255, 255, 0.1) 1px, transparent 1px),
+    linear-gradient(rgba(255, 255, 255, 0.05) 1px, transparent 1px),
+    linear-gradient(90deg, rgba(255, 255, 255, 0.05) 1px, transparent 1px);
+  background-size: 100px 100px, 100px 100px, 20px 20px, 20px 20px;
+  background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
+  background-attachment: fixed;
 }
-.spot-word{
-  font-size:80px;
+.spot-word {
+  font-size: 80px;
   color: #444444;
   font-family: fantasy;
   position: relative;
@@ -34,11 +44,11 @@ export default {
   display: inline-block;
   font-weight: bolder;
 }
-.spot-word::after{
-  content:attr(data-txt);
+.spot-word::after {
+  content: attr(data-txt);
   position: absolute;
   color: transparent;
-  font-size:80px;
+  font-size: 80px;
   height: 120px;
   line-height: 120px;
   left: 0;
@@ -46,7 +56,14 @@ export default {
   letter-spacing: 6px;
   clip-path: ellipse(50px 50px at 0% 50%);
   animation: spotlight 5s infinite ease-in-out;
-  background-image: radial-gradient( circle 1173px at -3.5% -4.8%,  rgba(255,140,16,1) 8.4%, rgba(255,26,26,1) 20.6%, rgba(0,64,249,1) 46.7%, rgba(131,201,8,1) 69.6%, rgba(255,230,28,1) 84.7% );
+  background-image: radial-gradient(
+    circle 1173px at -3.5% -4.8%,
+    rgba(255, 140, 16, 1) 8.4%,
+    rgba(255, 26, 26, 1) 20.6%,
+    rgba(0, 64, 249, 1) 46.7%,
+    rgba(131, 201, 8, 1) 69.6%,
+    rgba(255, 230, 28, 1) 84.7%
+  );
   background-size: 150%;
   background-position: center center;
   background-clip: text;
@@ -57,14 +74,13 @@ export default {
   0% {
     clip-path: ellipse(50px 50px at 0% 50%);
   }
-  
+
   50% {
     clip-path: ellipse(50px 50px at 100% 50%);
   }
-  
+
   100% {
     clip-path: ellipse(50px 50px at 0% 50%);
   }
 }
-
 </style>
