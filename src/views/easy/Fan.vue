7b22e35fc7fec3a4e339b1ecec8924ebac673ee6
jsmask <2674800533@qq.com>
2020-08-19 17:47:27 +0800

    update

--- src/views/easy/Fan.vue
+++ src/views/easy/Fan.vue
@@ -3,14 +3,14 @@
     <div class="fan">
       <div class="fan-body">
         <div class="fan-logo">
-            <span></span>
-            <span></span>
-            <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
         <div class="fan-logo">
-            <span></span>
-            <span></span>
-            <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
         <span class="fan-circle"></span>
         <span class="fan-circle"></span>
@@ -36,10 +36,8 @@
 </template>
 
 <style scoped>
-
-
 .main {
-  --bg-color:rgb(245, 231, 171);
+  --bg-color: rgb(245, 231, 171);
   --skin-color: rgb(219, 65, 67);
   --wood-color: rgb(62, 61, 59);
   --border-color: rgb(218, 197, 132);
@@ -60,7 +58,6 @@
 .fan {
   position: relative;
   transform: translateY(-25%);
-
 }
 .fan-body {
   width: 360px;
@@ -171,94 +168,100 @@
   top: 50%;
   transform: translate(-50%, -50%);
 }
-.fan-part:nth-of-type(2){
-    margin-left: 70px;
-    width: 15px;
-    margin-top: -10px;
-    transform: translate(-50%, -50%) rotate(-25deg);
+.fan-part:nth-of-type(2) {
+  margin-left: 70px;
+  width: 15px;
+  margin-top: -10px;
+  transform: translate(-50%, -50%) rotate(-25deg);
 }
-.fan-part:nth-of-type(3){
-    margin-left: -70px;
-    width: 15px;
-    margin-top: -10px;
-    transform: translate(-50%, -50%) rotate(25deg);
+.fan-part:nth-of-type(3) {
+  margin-left: -70px;
+  width: 15px;
+  margin-top: -10px;
+  transform: translate(-50%, -50%) rotate(25deg);
 }
-.fan-part:nth-of-type(4){
-    margin-left: 150px;
-    width: 15px;
-    margin-top: -30px;
-    transform: translate(-50%, -50%) rotate(-50deg);
+.fan-part:nth-of-type(4) {
+  margin-left: 150px;
+  width: 15px;
+  margin-top: -30px;
+  transform: translate(-50%, -50%) rotate(-50deg);
 }
-.fan-part:nth-of-type(5){
-    margin-left: -150px;
-    width: 15px;
-    margin-top: -30px;
-    transform: translate(-50%, -50%) rotate(50deg);
+.fan-part:nth-of-type(5) {
+  margin-left: -150px;
+  width: 15px;
+  margin-top: -30px;
+  transform: translate(-50%, -50%) rotate(50deg);
 }
 
 .fan-body::after {
-  /* display: none; */
   content: "祭";
-  top:-10px;
+  width: 100%;
+  height: 180px;
+  top: 50%;
+  transform: translateY(-80%);
   font-weight: bolder;
   font-size: 180px;
   color: var(--wood-color);
-  position: relative;
+  position: absolute;
   font-family: cursive;
   z-index: 99;
-  text-shadow: var(--circle-color) 1px 0 0, var(--circle-color) 0 1px 0, var(--circle-color) -1px 0 0, var(--circle-color) 0 -1px 0;
+  text-shadow: var(--circle-color) 1px 0 0, var(--circle-color) 0 1px 0,
+    var(--circle-color) -1px 0 0, var(--circle-color) 0 -1px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.fan-logo{
-    width: 136px;
-    height: 136px;
-    border-radius: 50%;
-    border: 2px solid var(--circle-color);
-    position: absolute;
-    top: 60px;
-    opacity: .9;
+.fan-logo {
+  width: 136px;
+  height: 136px;
+  border-radius: 50%;
+  border: 2px solid var(--circle-color);
+  position: absolute;
+  top: 60px;
+  opacity: 0.9;
 }
-.fan-logo:nth-of-type(1){
-    left: 30px;
+.fan-logo:nth-of-type(1) {
+  left: 30px;
 }
-.fan-logo:nth-of-type(2){
-    right: 30px;
+.fan-logo:nth-of-type(2) {
+  right: 30px;
 }
-.fan-logo>span{
-    display: block;
-    width: 32px;
-    height: 32px;
-    border-radius: 50%;
-    position: absolute;
-    background-color:var(--circle-color);
-    left: 50%;
-    top: 50%;
+.fan-logo > span {
+  display: block;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  position: absolute;
+  background-color: var(--circle-color);
+  left: 50%;
+  top: 50%;
 }
-.fan-logo>span::before{
-    content: '';
-    width: 56px;
-    height: 56px;
-    border-radius: 50%;
-    position: absolute;
-    border-top: 13px solid var(--circle-color);
-    border-right: 10px solid transparent;
-    transform: rotate(-120deg);
-    top: -12px;
-    right: -25px;
+.fan-logo > span::before {
+  content: "";
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  position: absolute;
+  border-top: 13px solid var(--circle-color);
+  border-right: 10px solid transparent;
+  transform: rotate(-120deg);
+  top: -12px;
+  right: -25px;
 }
-.fan-logo>span:nth-of-type(1){
-    margin-top: -52px;
-    margin-left: -16px;
-    transform:scale(1.2) rotate(90deg);
+.fan-logo > span:nth-of-type(1) {
+  margin-top: -52px;
+  margin-left: -16px;
+  transform: scale(1.2) rotate(90deg);
 }
-.fan-logo>span:nth-of-type(2){
-    margin-top: 8px;
-    margin-left: -40px;
-    transform: scale(1.2) rotate(-30deg);
+.fan-logo > span:nth-of-type(2) {
+  margin-top: 8px;
+  margin-left: -40px;
+  transform: scale(1.2) rotate(-30deg);
 }
-.fan-logo>span:nth-of-type(3){
-    margin-top: -2px;
-    margin-left: 20px;
-    transform: scale(1.2) rotate(-150deg);
+.fan-logo > span:nth-of-type(3) {
+  margin-top: -2px;
+  margin-left: 20px;
+  transform: scale(1.2) rotate(-150deg);
 }
 </style>
